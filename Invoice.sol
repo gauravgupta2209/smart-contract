40358d828112efc270e8cf1ec79086c38af61ef3
Gaurav <102946690+gauravgupta2209@users.noreply.github.com>
2022-08-05 21:45:37 +0530

    Add files via upload

---
+++ Invoice.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+contract invoice{
+
+    mapping(string => mapping(uint => task)) public show_invoice;
+
+    uint256 count = 0;
+    uint256 id;
+
+
+    struct task{
+        string SellerPAN;
+        uint256 Invoice_Amount;
+        string Invoice_Date;
+    }
+
+
+    enum Statuses { success, unpayed }
+    Statuses currentStatus;
+
+
+    constructor() public{
+        currentStatus = Statuses.unpayed;
+    }
+
+
+    function Enter_data(string memory _BuyerPAN, uint256 _id, string memory _SellerPAN, uint256 _Invoice_Amount, string memory _Invoice_Date) public{
+        show_invoice[_BuyerPAN][_id] = task(_SellerPAN , _Invoice_Amount ,_Invoice_Date);
+        currentStatus = Statuses.success;
+        count++;
+        id=_id;
+    }
+
+
+    function status(uint256 _id) view public returns(string memory){
+        for (uint256 i = 0; i < count; i++) { 
+            if (currentStatus == Statuses.success) {
+                if (_id == id){
+                    return "payed";
+                }
+                else{
+                    return "unpayed";
+                }
+            }
+        }
+    return "unpayed";
+    }
+    }
